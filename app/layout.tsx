9c81fefbada4fdbaf7df7d33e8ec8224cf92625f
Shacosu <sh4c0p@gmail.com>
2025-08-10 18:12:31 -0400

    feat: create root layout with Chatwoot integration and custom font setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Script from "next/script";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -26,6 +27,21 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${poppins.variable} antialiased bg-black relative`}>
+        <Script id="chatwoot" strategy="afterInteractive">
+          {`(function(d,t){
+  var BASE_URL="https://chatwoot.cloudnext.cl";
+  var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+  g.src=BASE_URL+"/packs/js/sdk.js";
+  g.async=true;
+  s.parentNode.insertBefore(g,s);
+  g.onload=function(){
+    window.chatwootSDK.run({
+      websiteToken: 'NJojX6BwdZVC1hNFoAb8f2QA',
+      baseUrl: BASE_URL
+    })
+  }
+})(document,"script");`}
+        </Script>
         <div className="pointer-events-none fixed inset-0 z-[-1] bg-[url('/images/noise.png')] opacity-3 mix-blend-screen" />
         <div className="z-10">
           <Navbar />
